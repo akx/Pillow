7dc34fd9e760f33e83cdf31165ea3e19f3f4824c
Aarni Koskela <akx@iki.fi>
2023-02-22 10:40:55 +0200

    CurImagePlugin: piggy-back on IcoImagePlugin for better format support

--- Tests/test_file_cur.py
+++ Tests/test_file_cur.py
@@ -19,3 +19,12 @@ def test_invalid_file():
 
     with pytest.raises(SyntaxError, match="No images were found"):
         CurImagePlugin.CurImageFile("Tests/images/no_cursors.cur")
+
+
+def test_1bit_transparency():
+    with Image.open("Tests/images/cur_259.cur") as im:
+        assert im.size == (32, 32) and im.mode == "RGBA"
+        assert im.getpixel((0, 0)) == (0, 0, 0, 255)  # pointy bit is black
+        assert im.getpixel((1, 0)) == (0, 0, 0, 255)  # and its neighbor is black
+        assert im.getpixel((2, 0)) == (0, 0, 0, 0)  # third pixel in the upper row is transparent though
+        assert im.getpixel((1, 1)) == (255, 255, 255, 255)  # cursor body is white
--- src/PIL/CurImagePlugin.py
+++ src/PIL/CurImagePlugin.py
@@ -28,44 +28,30 @@ def _accept(prefix):
     return prefix[:4] == b"\0\0\2\0"
 
 
-##
-# Image plugin for Windows Cursor files.
-
-
-class CurImageFile(BmpImagePlugin.BmpImageFile):
-    format = "CUR"
-    format_description = "Windows Cursor"
-
-    def _open(self):
-        offset = self.fp.tell()
-
-        # check magic
-        s = self.fp.read(6)
-        if not _accept(s):
+class CurFile(IcoImagePlugin.IcoFile):
+    def _check_header(self, header):
+        if not _accept(header):
             msg = "not a CUR file"
             raise SyntaxError(msg)
 
-        # pick the largest cursor in the file
-        m = b""
-        for i in range(i16(s, 4)):
-            s = self.fp.read(16)
-            if not m:
-                m = s
-            elif s[0] > m[0] and s[1] > m[1]:
-                m = s
-        if not m:
-            msg = "No cursors were found"
-            raise TypeError(msg)
+    def _read_icon_header(self, buf) -> dict:
+        header = super()._read_icon_header(buf)
+        # The planes/bpp fields are used for hotspot coordinates in cursors,
+        # so rename them in the header to avoid confusion; removing `bpp`
+        # (which would be a bogus value for a CUR) affects how IcoFile.frame()
+        # determines a given sub-image's mask.
+        header["hotspot_x"] = header.pop("planes")
+        header["hotspot_y"] = header.pop("bpp")
+        return header
 
-        # load as bitmap
-        self._bitmap(i32(m, 12) + offset)
 
-        # patch up the bitmap height
-        self._size = self.size[0], self.size[1] // 2
-        d, e, o, a = self.tile[0]
-        self.tile[0] = d, (0, 0) + self.size, o, a
-
-        return
+class CurImageFile(IcoImagePlugin.IcoImageFile):
+    """
+    PIL read-only image support for Microsoft Windows .cur files.
+    """
+    format = "CUR"
+    format_description = "Windows Cursor"
+    _ico_file_class = CurFile
 
 
 #
