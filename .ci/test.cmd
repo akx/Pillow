2cee23885538d5f0be5c7d1e5967a25eae53d683
Andrew Murray <radarhere@users.noreply.github.com>
2026-09-09 15:58:12 +1000

    Do not run isolated and non-isolated tests separately

--- .ci/test.cmd
+++ .ci/test.cmd
@@ -1,5 +1,3 @@
 python.exe -c "from PIL import Image"
 IF ERRORLEVEL 1 EXIT /B
-python.exe -bb -m pytest -vv -x -W always Tests -m "isolated"
-IF ERRORLEVEL 1 EXIT /B
-python.exe -bb -m pytest -vv -x -W always Tests -m "not isolated" --numprocesses=logical --dist=worksteal --cov PIL --cov Tests --cov-report term --cov-report xml
+python.exe -bb -m pytest -vv -x -W always Tests --numprocesses=logical --dist=worksteal --cov PIL --cov Tests --cov-report term --cov-report xml
--- .ci/test.sh
+++ .ci/test.sh
@@ -4,7 +4,6 @@ set -e
 
 python3 -c "from PIL import Image"
 
-python3 -bb -m pytest -vv -x -W always Tests -m "isolated"
-python3 -bb -m pytest -vv -x -W always Tests -m "not isolated" \
+python3 -bb -m pytest -vv -x -W always Tests \
   --numprocesses=logical --dist=worksteal \
   --cov PIL --cov Tests --cov-report term --cov-report xml
